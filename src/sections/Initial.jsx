bf54df9a731b49902c405fa1c8b930e74a455399
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-11-02 14:05:36 -0300

    section Initial finalized - lack of responsiveness - start section about

--- src/sections/Initial.jsx
+++ src/sections/Initial.jsx
@@ -1,7 +1,29 @@
+import styles from "../styles/initial.module.scss"
+
+
+
 const Initial = () =>(
-   <>
-     Initial
-   </>
+   <div className={styles.container}>
+    <div className={styles.leftBox}>
+      <div className={styles.textBox}>
+        <p className={styles.logo}>VIVACE</p>
+        <p className={styles.title}>O Descanso Que Você Merece</p>
+        <p className={styles.subTitle}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laud.</p>
+        <button className={styles.btnLeft}>FAÇA SUA RESERVE</button>
+      </div>
+    </div>
+    <div className={styles.rightBox}>
+        <div className={styles.navBar}>
+           <a href="#">Sobre</a>
+           <a href="#">Quartos</a>
+           <a href="#">Contato</a>
+           <button className={styles.btnRight}>RESERVAR</button>
+        </div>
+        <div className={styles.boxImg}/>  
+        
+        
+    </div>
+   </div>
 );
 
 export default Initial
