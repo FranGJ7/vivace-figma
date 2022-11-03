21726eff11207150c3f74f3345db37cee95f2a9a
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-11-02 23:21:25 -0300

    section 'contact' finalized - lack of responsiveness

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -1,7 +1,23 @@
-const Contact = () =>(
-    <>
-      Contact
-    </>
- );
- 
- export default Contact
+import styles from "../styles/contact.module.scss"
+
+const Contact = () => (
+  <div className={styles.contactSection}>
+    <div className={styles.form}>
+      <p className={styles.title}>Entre em contato e faça sua reserva</p>
+      <hr />
+      <label htmlFor="name" >Name</label>
+      <input id="name" type="text" placeholder="Ex: João martins souza" />
+      <label htmlFor="email" >Email</label>
+      <input id="email" type="email" placeholder="Ex: joao@gmail.com" />
+      <label htmlFor="message">Mensagem</label>
+      <input id="message" type="text" placeholder="Mensagem" />
+      <button>Enviar</button>
+    </div>
+    <div className={styles.boxImg}>
+      <img src="../../public/img-inn/contact.png" alt="" />
+
+    </div>
+  </div>
+);
+
+export default Contact
