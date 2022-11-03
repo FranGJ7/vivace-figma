09c6407d50ede868dd2f87235a6d64047eabc592
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-11-02 22:35:04 -0300

    footer finalized

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -1,8 +1,11 @@
+import styles from "../styles/footer.module.scss"
+
 const Footer = () =>(
-    <>
-      Footer
+ 
+    <div className={styles.footer}>
+        <p className={styles.copyRight}>© Todos os direitos reservados. Vivace 2022 </p>
+    </div>
     
-    </>
  );
  
  export default Footer
